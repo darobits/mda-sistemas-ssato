d834db9afd637b65b225bb76300165f83ca05e47
darobits <villardario.e@gmail.com>
2026-07-03 05:57:39 -0300

    final config app script

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -1,1 +1,1 @@
-export const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwdMwMt_iemZZHl__Ys-bCskuqNurCioNf7KVLUJ1XQJK6Gfi2vZWx1UlpRmSLh-78OFA/exec';
+export const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzp67lOTGMNjc796OMH52H47aQz-w5qdO90V7g7NpJH9a493eTz8ksNfxCNxmwFDiHYXA/exec';
