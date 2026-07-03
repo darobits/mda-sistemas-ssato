8940550becf618e031d3b57654773f5b8cf8a50e
darobits <villardario.e@gmail.com>
2026-07-03 00:27:28 -0300

    Actualizar favicon SSATO

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -1,1 +1,1 @@
-export const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycby3z2oIKfnqQVEatM6XxfL5MbNYlJfElZRAaCgMMp7Th6EzVlXpADsb1E2a9PMPYPQ-ew/exec';
+export const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycby0GQTWqFRI1yoYOMUph7wzMJXNqHM9K3LI6zB-hDQAeGI3DNTKcvaA5PKBqWnouriu6A/exec';
