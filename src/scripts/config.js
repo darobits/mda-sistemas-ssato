ad9929b444f8ab7d612869224396e20587a9d95e
darobits <villardario.e@gmail.com>
2026-07-03 05:24:50 -0300

    cambio de app script en sheets

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -1,1 +1,1 @@
-export const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycby9xvFWfr0upI3X8aHNIuEnt-e4D-Mz54DSLu3tR0vxTXngZjM-ciHfA3u-5G9w0td2YQ/exec';
+export const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxOUFKyylu39ubZ0qSRsmRgXrkrpMPrD6nL_kYbcUZfoItcUsqBSYwpyC8QnC9BOww4UQ/exec';
